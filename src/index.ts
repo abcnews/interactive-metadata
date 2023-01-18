96eadfcfc0ebcb4b82d597ab3c280c439dab76a1
Colin Gourlay <colin@colin-gourlay.com>
2023-01-19 08:10:21 +1000

    Pare back. Namespace mount points.

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,26 @@
-import acto from '@abcnews/alternating-case-to-object';
 import { whenOdysseyLoaded } from '@abcnews/env-utils';
 import { getMountValue, selectMounts } from '@abcnews/mount-utils';
-import type { Mount } from '@abcnews/mount-utils';
+import type { ComponentType } from 'svelte/internal';
 import App from './components/App/App.svelte';
 
-let appMountEl: Mount;
-let appProps;
-
 whenOdysseyLoaded.then(() => {
-  [appMountEl] = selectMounts('interactivemetadata');
+  selectMounts('metadata').forEach(mountEl => {
+    const [, componentName] = getMountValue(mountEl).split(':');
+    let Component: ComponentType | null = null;
 
-  if (appMountEl) {
-    appProps = acto(getMountValue(appMountEl));
-    new App({
-      target: appMountEl,
-      props: appProps
-    });
-  }
+    switch (componentName) {
+      case 'app':
+        Component = App;
+        break;
+      default:
+        console.debug(`[metadata] Unrecognised component name: ${componentName}`);
+    }
+
+    if (Component) {
+      new Component({
+        target: mountEl,
+        props: {}
+      });
+    }
+  });
 });
-
-if (process.env.NODE_ENV === 'development') {
-  console.debug(`[interactive-metadata] public path: ${__webpack_public_path__}`);
-}
