ee477cb598afb78967bfb146150950057af0c6cc
Colin Gourlay <colin@colin-gourlay.com>
2023-01-19 09:05:21 +1000

    Add Download component

---
+++ src/components/Download/Download.svelte
@@ -0,0 +1,7 @@
+<script>
+  const FILE_URL = `${__webpack_public_path__}data/metadata.csv`;
+</script>
+
+<div>
+  <a href={FILE_URL} download="will-ockenden-metadata.csv">Download Will's metadata (CSV)</a>
+</div>
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { whenOdysseyLoaded } from '@abcnews/env-utils';
 import { getMountValue, selectMounts } from '@abcnews/mount-utils';
 import type { ComponentType } from 'svelte/internal';
 import App from './components/App/App.svelte';
+import Download from './components/Download/Download.svelte';
 
 whenOdysseyLoaded.then(() => {
   selectMounts('metadata').forEach(mountEl => {
@@ -10,10 +11,20 @@ whenOdysseyLoaded.then(() => {
 
     switch (componentName) {
       case 'all-days':
+        Component = App;
+        break;
       case 'contacts-quiz':
+        Component = App;
+        break;
       case 'daily-route':
+        Component = App;
+        break;
       case 'download':
+        Component = Download;
+        break;
       case 'heat-interpolated':
+        Component = App;
+        break;
       case 'popular-contacts':
         Component = App;
         break;
